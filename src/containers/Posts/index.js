4753beda4345fc9839bd92c1a025f2554da791b5
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-04-29 08:46:50 -0400

    removed old collapse code

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -3,20 +3,6 @@ import { Badge, Button, Card, CardBody, CardHeader, CardText, Collapse } from 'r
 
 
 class Posts extends Component {
-  constructor() {
-    super();
-    // this.state = { collapse: false };
-    this.toggle = this.toggle.bind(this);
-  }
-
-  state = { collapse: {} };
-
-  toggle = (key) => {
-    var old = {...this.state.collapse};
-    old[key] = !old[key];
-    this.setState({ collapse: old }, () => console.log(this.state.collapse));
-  }
-
   render() {
     let posts = this.props.posts;
     let _this = this;
@@ -39,23 +25,18 @@ class Posts extends Component {
         <div className="Posts">
           <br/>
           { Object.keys(posts).map(function(key) {
-              let old = {..._this.state.collapse};
-              old[key] = true;
-              _this.state.collapse = old;
               let tags = posts[key].tags;
               return (
                 <div key={key}>
                   <Card>
-                    <CardHeader onClick={ (e) => _this.toggle(key) }>
+                    <CardHeader>
                       {tags.map(function(keyTag, i) {
                         return (<h7 key={i}><Badge color="secondary">{keyTag}</Badge>&nbsp;</h7>)
                       })}
                     </CardHeader>
-                    <Collapse isOpen={_this.state.collapse[key]}>
-                      <CardBody>
-                        <CardText>{ posts[key].title }</CardText>
-                      </CardBody>
-                    </Collapse>
+                    <CardBody>
+                      <CardText>{ posts[key].title }</CardText>
+                    </CardBody>
                   </Card>
                   <br/>
                 </div>
